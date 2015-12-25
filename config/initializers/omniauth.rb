73775e7ffbbb8ad15bfb8cd0bab2169da741bf18
Timilehin Aderinola <timiderinola@gmail.com>
2015-12-25 17:33:54 +0100

    Edit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.full_host = Rails.env.production? ? 'https://christpraisechapel.herokuapp.com' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], scope: 'userinfo.profile, youtube'
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: 'userinfo.profile, youtube'
   # provider :google_oauth2, ENV['287422847911-0jndenitrsdg4it0h8gqgmvb31sv7rq7.apps.googleusercontent.com'], ENV['41EsgtdjFJ2FJ98tJVzttSGE'], scope: 'userinfo.profile,youtube'
 end
 
