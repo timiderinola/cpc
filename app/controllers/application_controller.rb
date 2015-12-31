716630f2a3cb9f5547e1f6ac74b7fcbdcee22d6d
Timilehin Aderinola <timiderinola@gmail.com>
2015-12-31 22:51:32 +0100

    edited application_controller to remove ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_filter :expire_hsts
+
+  private
+  def expire_hsts
+    response.headers['Strict-Transport-Security'] = 'max-age=0'
+  end
 end
