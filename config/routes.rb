973550ec6b51231937d754b11893b648c863a2a2
Timilehin Aderinola <timiderinola@gmail.com>
2016-01-27 13:15:21 +0100

    Some few changes

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,6 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'messages/:title' => 'messages#show', as: :title
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
